2648e2810460e0d4183cc3100c17c6eb12e2bfd0
Allison Baker <abaker@ucar.edu>
2025-10-15 12:18:44 -0600

    matvec interface

--- f_pdgsmv.c
+++ f_pdgsmv.c
@@ -1,8 +1,15 @@
-#include "superlu_ddefs.h"
 
-void f_pdgsmv(void *opt, void *A, double *x, double *y, void *grid)
+
+#include "superlu_ddefs.h"
+#include "Cnames.h"
+
+void f_pdgsmv(int_t *n, void *A, void *grid, double *x, double *y)
 {
-    pdgsmv((superlu_dist_options_t *)opt,
-           (SuperMatrix *)A, x, y,
-           (gridinfo_t *)grid);
+    pdgsmv_comm_t gsmv_comm;
+
+    dInit_GSMV_comm((SuperMatrix *)A, (gridinfo_t *)grid, &gsmv_comm);
+
+    pdgsmv(*n, (SuperMatrix *)A, (gridinfo_t *)grid, &gsmv_comm, x, y);
+
+    dDestroy_GSMV_comm(&gsmv_comm);
 }
--- superlu_bindings.F90
+++ superlu_bindings.F90
@@ -1,11 +1,16 @@
 module superlu_bindings
 
   interface
-     subroutine f_pdgsmv(options, A, x, y, grid) bind(c, name='f_pdgsmv')
+     subroutine f_pdgsmv(n, A, grid, x, y) bind(C, name="f_pdgsmv")
        use iso_c_binding
-       type(c_ptr), value :: options, A, grid
-       real(c_double)     :: x(*), y(*)
+       implicit none
+       integer(c_int64_t), value :: n   ! matches C int_t
+       type(c_ptr), value :: A          ! pointer to SuperMatrix
+       type(c_ptr), value :: grid       ! pointer to gridinfo_t
+       real(c_double) :: x(*)           ! input vector
+       real(c_double) :: y(*)           ! output vector
      end subroutine f_pdgsmv
   end interface
 
+  
 endmodule superlu_bindings
