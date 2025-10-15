55d5f13cfe7f7938eab725974383b8170e9e3335
Allison Baker <abaker@ucar.edu>
2025-10-15 14:09:22 -0600

    matvec interface

--- superlu_bindings.F90
+++ superlu_bindings.F90
@@ -1,7 +1,7 @@
 module superlu_bindings
 
   interface
-    subroutine f_pdgsmv(n, A_handle, grid_handle, x, y) bind(C, name="f_pdgsmv_handle")
+    subroutine f_pdgsmv(n, A_handle, grid_handle, x, y) bind(C, name="f_pdgsmv")
         use iso_c_binding
         implicit none
         integer(c_int64_t), value :: n
