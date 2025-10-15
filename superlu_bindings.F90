ae3d86056908cf3bf04a2de3d49e45fce559e8e1
Allison Baker <abaker@ucar.edu>
2025-10-15 14:05:54 -0600

    matvec interface

--- superlu_bindings.F90
+++ superlu_bindings.F90
@@ -1,17 +1,16 @@
 module superlu_bindings
 
   interface
-     subroutine f_pdgsmv(n, A, grid, x, y) bind(C, name="f_pdgsmv")
-       use iso_c_binding
-       implicit none
-       integer(c_int64_t), value :: n   ! matches C int_t
-       type(c_ptr), value :: A          ! pointer to SuperMatrix
-       type(c_ptr), value :: grid       ! pointer to gridinfo_t
-       real(c_double) :: x(*)           ! input vector
-       real(c_double) :: y(*)           ! output vector
-     end subroutine f_pdgsmv
-  end interface
+    subroutine f_pdgsmv(n, A_handle, grid_handle, x, y) bind(C, name="f_pdgsmv_handle")
+        use iso_c_binding
+        implicit none
+        integer(c_int64_t), value :: n
+        integer(c_int64_t), value :: A_handle
+        integer(c_int64_t), value :: grid_handle
+        real(c_double) :: x(*)
+        real(c_double) :: y(*)
+    end subroutine f_pdgsmv_handle
+end interface
 
-  
 endmodule superlu_bindings
 
