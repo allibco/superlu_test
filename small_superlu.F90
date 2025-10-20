456a1ba8a73fb9de87363e6739daa006b2f5d7b0
Allison Baker <abaker@ucar.edu>
2025-10-20 13:08:52 -0600

    working on matvec

--- small_superlu.F90
+++ small_superlu.F90
@@ -101,26 +101,25 @@ program small_superlu
 
 
 ! Print some metadata
-  print *, 'iam = ' iam, 'halo rank =', halo%rank
-  print *, 'iam = ' iam,'nprocs_local =', halo%nprocs_local
-  print *, 'iam = ' iam,'n_global =', halo%n_global
-  print *, 'iam = ' iam,'m_loc =', halo%m_loc
-  print *, 'iam = ' iam,'fst_row =', halo%fst_row, 'last_row =', halo%last_row
-  print *, 'iam = ' iam,'nhalo =', halo%nhalo
+  print *, 'iam = ', iam, 'halo rank =', halo%rank
+  print *, 'iam = ', iam,'nprocs_local =', halo%nprocs_local
+  print *, 'iam = ', iam,'n_global =', halo%n_global
+  print *, 'iam = ', iam,'m_loc =', halo%m_loc
+  print *, 'iam = ', iam,'fst_row =', halo%fst_row, 'last_row =', halo%last_row
+  print *, 'iam = ', iam,'nhalo =', halo%nhalo
 
 ! Print integer arrays (halo columns and owners)
   if (halo%nhalo > 0) then
-     print *, 'iam = ' iam,  'halo_cols:', halo%halo_cols(1:halo%nhalo)
-     print *, 'iam = ' iam, 'owners:', halo%owners(1:halo%nhalo)
+     print *, 'iam = ', iam,  'halo_cols:', halo%halo_cols(1:halo%nhalo)
+     print *, 'iam = ', iam, 'owners:', halo%owners(1:halo%nhalo)
   end if
 
 ! Print send/receive counts
-  print *, 'iam = ' iam,'sendcounts:', halo%sendcounts
-  print *, 'iam = ' iam,'sdispls:', halo%sdispls
-  print *, 'iam = ' iam,'recvcounts:', halo%recvcounts
-  print *, 'iam = ' iam,'rdispls:', halo%rdispls
-  print *, 'iam = ' iam,'recv_from:', halo%recv_from
-
+  print *, 'iam = ', iam,'sendcounts:', halo%sendcounts
+  print *, 'iam = ', iam,'sdispls:', halo%sdispls
+  print *, 'iam = ', iam,'recvcounts:', halo%recvcounts
+  print *, 'iam = ', iam,'rdispls:', halo%rdispls
+  print *, 'iam = ', iam,'recv_from:', halo%recv_from
 
   
   !print *, "Rank", iam, "m_loc=", m_loc, "nnz_loc=", nnz_loc, "fst_row=", fst_row
